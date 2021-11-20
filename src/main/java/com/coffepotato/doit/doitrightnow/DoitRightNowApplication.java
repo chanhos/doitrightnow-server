07c456333a80a985e10885eda89fbe6beef2214e
clark <chjung@lunasoft.co.kr>
2021-11-21 00:36:56 +0900

    initial commit

---
+++ src/main/java/com/coffepotato/doit/doitrightnow/DoitRightNowApplication.java
@@ -0,0 +1,13 @@
+package com.coffepotato.doit.doitrightnow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoitRightNowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoitRightNowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/coffepotato/doit/doitrightnow/DoitRightNowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coffepotato.doit.doitrightnow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoitRightNowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
